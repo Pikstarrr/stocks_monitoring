68011d0fcda203a0b7e13e802be703d9d791082a
techlead <prasad@etechtracker.com>
2025-01-20 14:37:01 +0530

    Changes -
    Added difference percent to check current different between high and low.

--- script.py
+++ script.py
@@ -56,17 +56,23 @@ def update_value_in_db_and_user():
     doc = doc_ref.get()
     current_highest_value = doc.to_dict().get("highest_value", 0.0) if doc.exists else 0.0
 
+    difference_percent = (current_high_value - current_low_value) / current_high_value
     if current_high_value > current_highest_value:
         current_highest_value = current_high_value
         doc_ref.set({"highest_value": current_highest_value})
-    elif (current_highest_value - current_low_value) / current_highest_value > 0.05:
+        doc_ref.set({"difference_percent": 0.0})
+
+    elif difference_percent > 0.04:
         send_email(
             "Stock Alert: Significant Drop",
             f"Current Value: {current_low_value}, Highest Value: {current_highest_value}",
         )
         doc_ref.set({"highest_value": current_highest_value})
+        doc_ref.set({"difference_percent": difference_percent})
+    else:
+        doc_ref.set({"difference_percent": difference_percent})
 
-    print(current_high_value, current_high_value, current_highest_value)
+    print(current_high_value, current_high_value, current_highest_value, difference_percent)
 
 
 # Run Script
